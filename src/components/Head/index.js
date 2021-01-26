1a02f82ea2555c718e3c01f59bbd4e2d1299232e
Rodrigo Lemos <rodrigosllemos@gmail.com>
2021-01-26 00:30:01 -0300

    codigo ao final da primeira aula da imersao

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -4,7 +4,11 @@ function HeadPage() {
     return (
         <head>
             <title>Quiz 4 Estações</title>
-            <meta property="og:image" content="https://wallpapercave.com/wp/wp3973339.jpg"></meta>
+            <meta property="og:url" content="https://alura-quiz-seasons.rodlemos.vercel.app/" />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content="Quiz 4 Estações" />
+            <meta property="og:image" content="https://wallpapercave.com/wp/wp3973339.jpg" />
+
             <link href="https://fonts.googleapis.com/css2?family=Dosis&display=swap" rel="stylesheet"></link>
         </head>
     );
