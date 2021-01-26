207487336528c7fcbb835ff55a08abb6e8e05079
Rodrigo Lemos <rodrigosllemos@gmail.com>
2021-01-25 23:37:32 -0300

    codigo ao final da primeira aula da imersao

---
+++ src/components/Head/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+function HeadPage() {
+    return (
+        <head>
+            <title>Quiz 4 Estações</title>
+            <meta property="og:image" content="https://wallpapercave.com/wp/wp3973339.jpg"></meta>
+            <link href="https://fonts.googleapis.com/css2?family=Dosis&display=swap" rel="stylesheet"></link>
+        </head>
+    );
+}
+
+export default HeadPage;
